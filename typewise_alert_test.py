bd4e0b2aaf5e0c61bddc4d004b17815b3c869c4d
Uday612 <69905522+Uday612@users.noreply.github.com>
2024-08-25 20:38:08 +0530

    Update typewise_alert_test.py

--- typewise_alert_test.py
+++ typewise_alert_test.py
@@ -1,28 +1,29 @@
 import unittest
-import typewise_alert
+from unittest.mock import patch
+from typewise_alert import infer_breach, get_upper_limit, classify_temperature_breach, check_and_alert, send_to_controller, send_to_email, print_message
 
 
 class TypewiseTest(unittest.TestCase):
   def test_infer_breach(self):
-    self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
-    self.assertTrue(typewise_alert.infer_breach(50, 50, 100) == 'NORMAL')
-    self.assertTrue(typewise_alert.infer_breach(110, 50, 100) == 'TOO_HIGH')
+    self.assertTrue(infer_breach(20, 50, 100) == 'TOO_LOW')
+    self.assertTrue(infer_breach(50, 50, 100) == 'NORMAL')
+    self.assertTrue(infer_breach(110, 50, 100) == 'TOO_HIGH')
 
   def test_get_upper_limit(self):
-    self.assertTrue(typewise_alert.get_upper_limit('PASSIVE_COOLING') == 35)
-    self.assertTrue(typewise_alert.get_upper_limit('HI_ACTIVE_COOLING') == 45)
-    self.assertTrue(typewise_alert.get_upper_limit('MED_ACTIVE_COOLING') == 40)
+    self.assertTrue(get_upper_limit('PASSIVE_COOLING') == 35)
+    self.assertTrue(get_upper_limit('HI_ACTIVE_COOLING') == 45)
+    self.assertTrue(get_upper_limit('MED_ACTIVE_COOLING') == 40)
 
   def test_classify_temperature_breach(self):
-    self.assertTrue(typewise_alert.classify_temperature_breach("PASSIVE_COOLING",35) == 'NORMAL')
-    self.assertTrue(typewise_alert.classify_temperature_breach("HI_ACTIVE_COOLING",45) == 'NORMAL')
-    self.assertTrue(typewise_alert.classify_temperature_breach("MED_ACTIVE_COOLING",40) == 'NORMAL')
-    self.assertTrue(typewise_alert.classify_temperature_breach("PASSIVE_COOLING",-1) == 'TOO_LOW')
-    self.assertTrue(typewise_alert.classify_temperature_breach("HI_ACTIVE_COOLING",-2) == 'TOO_LOW')
-    self.assertTrue(typewise_alert.classify_temperature_breach("MED_ACTIVE_COOLING",-3) == 'TOO_LOW')
-    self.assertTrue(typewise_alert.classify_temperature_breach("PASSIVE_COOLING",40) == 'TOO_HIGH')
-    self.assertTrue(typewise_alert.classify_temperature_breach("HI_ACTIVE_COOLING",50) == 'TOO_HIGH')
-    self.assertTrue(typewise_alert.classify_temperature_breach("MED_ACTIVE_COOLING",45) == 'TOO_HIGH')
+    self.assertTrue(classify_temperature_breach("PASSIVE_COOLING",35) == 'NORMAL')
+    self.assertTrue(classify_temperature_breach("HI_ACTIVE_COOLING",45) == 'NORMAL')
+    self.assertTrue(classify_temperature_breach("MED_ACTIVE_COOLING",40) == 'NORMAL')
+    self.assertTrue(classify_temperature_breach("PASSIVE_COOLING",-1) == 'TOO_LOW')
+    self.assertTrue(classify_temperature_breach("HI_ACTIVE_COOLING",-2) == 'TOO_LOW')
+    self.assertTrue(classify_temperature_breach("MED_ACTIVE_COOLING",-3) == 'TOO_LOW')
+    self.assertTrue(classify_temperature_breach("PASSIVE_COOLING",40) == 'TOO_HIGH')
+    self.assertTrue(classify_temperature_breach("HI_ACTIVE_COOLING",50) == 'TOO_HIGH')
+    self.assertTrue(classify_temperature_breach("MED_ACTIVE_COOLING",45) == 'TOO_HIGH')
 
   @patch('__main__.print_message')
   def test_send_to_controller_normal(self, mock_print_message):
@@ -31,7 +32,7 @@ class TypewiseTest(unittest.TestCase):
 
   @patch('__main__.print_message')
   def test_send_to_controller_too_high(self, mock_print_message):
-    end_to_controller('TOO_HIGH')
+    send_to_controller('TOO_HIGH')
     mock_print_message.assert_called_with('65261, TOO_HIGH')
 
   @patch('__main__.print_message')
